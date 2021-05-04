09cb46563bf2070581ea1c1fa8020139e0c1877b
Ivan Viter <ivanviter@i.ua>
2021-05-04 16:52:31 +0300

    Add rubocop, rest-client

--- lib/weather_handler.rb
+++ lib/weather_handler.rb
@@ -1,7 +1,18 @@
-require "weather_handler/version"
+require 'weather_handler/version'
+require 'rest-client'
 
 module WeatherHandler
   class Error < StandardError; end
 
-  
+  class Weather
+
+    def find_weather(city)
+
+    end
+
+    private
+    
+    attr_reader :city
+
+  end
 end
