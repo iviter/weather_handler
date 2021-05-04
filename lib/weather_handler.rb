022a1e32352d848c3c093beeeeaeb4de58210cb8
Ivan Viter <ivanviter@i.ua>
2021-05-04 18:19:34 +0300

    Add basic functionality

--- lib/weather_handler.rb
+++ lib/weather_handler.rb
@@ -1,18 +1,40 @@
-require 'weather_handler/version'
+#require 'weather_handler/version'
 require 'rest-client'
+require 'dotenv/load'
+require 'json'
 
 module WeatherHandler
   class Error < StandardError; end
 
   class Weather
+    def initialize(city)
+      @city = city
+    end
 
-    def find_weather(city)
+    CONVERTATION_VALUE = 273.15.freeze
 
+    def find_weather
+      degrees
     end
 
     private
     
-    attr_reader :city
+    attr_reader :city, :weather_data
 
+    def weather_data
+      @weather_data ||= RestClient.get(location_url)
+    end
+
+    def location_url
+      "api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV['OPEN_WEATHER_API_KEY']}"
+    end
+
+    def weather_description
+      JSON.parse(weather_data.body).dig('weather').first['description'].split.map(&:capitalize).join(' ')
+    end
+
+    def degrees
+      (JSON.parse(weather_data.body).dig('main')['temp'] - CONVERTATION_VALUE).round(1)
+    end
   end
 end
--- spec/weather_handler_spec.rb
+++ spec/weather_handler_spec.rb
@@ -1,9 +1,7 @@
 RSpec.describe WeatherHandler do
-  it "has a version number" do
-    expect(WeatherHandler::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'constants' do
+    it 'sets CONVERTATION_VALUE constant' do
+      expect(described_class::Weather::CONVERTATION_VALUE).to eq 273.15
+    end
   end
 end
