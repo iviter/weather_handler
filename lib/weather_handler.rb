16c765020412d02670e9f49819727aa60de7cb09
Ivan Viter <ivanviter@i.ua>
2021-05-04 15:24:38 +0300

    Initialize gem scaffold

---
+++ lib/weather_handler.rb
@@ -0,0 +1,6 @@
+require "weather_handler/version"
+
+module WeatherHandler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/weather_handler/version.rb
@@ -0,0 +1,3 @@
+module WeatherHandler
+  VERSION = "0.1.0"
+end
