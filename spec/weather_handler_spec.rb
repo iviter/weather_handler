c1d2795205360b1e2d035d754d884788ec0b8f85
Ivan Viter <ivanviter@i.ua>
2021-05-08 20:24:56 +0300

    Add specs

--- spec/weather_handler_spec.rb
+++ spec/weather_handler_spec.rb
@@ -1,7 +1,16 @@
-RSpec.describe WeatherHandler::Weather do
-  describe 'constants' do
-    let(:error_message) { 'Please, try to type correct dimension. Correct types are: Celsium, Kelvin and Fahrenheit.' }
+# frozen_string_literal: true
 
+RSpec.describe WeatherHandler::Weather do
+  let(:lviv_city) { 'Lviv' }
+  let(:celsium) { 'celsium' }
+  let(:kelvin) { 'kelvin' }
+  let(:fahrenheit) { 'fahrenheit' }
+  let(:inch) { 'inch' }
+  let(:error_message) { 'Please, try to type correct dimension. Correct types are: Celsium, Kelvin and Fahrenheit.' }
+
+  subject { described_class.new(lviv_city) }
+
+  describe 'constants' do
     it 'sets CONVERTATION_KELVIN_VALUE constant' do
       expect(described_class::CONVERTATION_KELVIN_VALUE).to eq 273.15
     end
@@ -19,46 +28,135 @@ RSpec.describe WeatherHandler::Weather do
     end
 
     it 'sets CELSIUM constant' do
-      expect(described_class::CELSIUM).to eq 'celsium'
+      expect(described_class::CELSIUM).to eq celsium
     end
 
     it 'sets KELVIN constant' do
-      expect(described_class::KELVIN).to eq 'kelvin'
+      expect(described_class::KELVIN).to eq kelvin
     end
 
     it 'sets FAHRENHEIT constant' do
-      expect(described_class::FAHRENHEIT).to eq 'fahrenheit'
+      expect(described_class::FAHRENHEIT).to eq fahrenheit
     end
   end
 
   describe '#new' do
-    it 'creates new instance of WeatherHandler::Weather class' do
-      expect(described_class.new('Lviv')).to be_a described_class
+    context 'with correct amount of arguments' do
+      it 'creates new instance of WeatherHandler::Weather class' do
+        expect(subject).to be_a described_class
+      end
     end
 
-    it 'works' do
-      byebug
-      expect { described_class.new }.to raise_error
+    context 'with wrong amount of arguments' do
+      it 'raises ArgumentError' do
+        expect { described_class.new }.to raise_error(ArgumentError)
+      end
     end
   end
 
   describe '#current_temperature' do
-    
+    context 'with celsium dimension' do
+      it 'returns result with a string type' do
+        expect(subject.current_temperature(celsium)).to be_a String
+      end
+
+      it 'returns truthy result' do
+        expect(subject.current_temperature(celsium)).not_to be_nil
+      end
+    end
+
+    context 'with kelvin dimension' do
+      it 'returns result with a string type' do
+        expect(subject.current_temperature(kelvin)).to be_a String
+      end
+
+      it 'returns truthy result' do
+        expect(subject.current_temperature(kelvin)).not_to be_nil
+      end
+    end
+
+    context 'with fahrenheit dimension' do
+      it 'returns result with a string type' do
+        expect(subject.current_temperature(fahrenheit)).to be_a String
+      end
+
+      it 'returns truthy result' do
+        expect(subject.current_temperature(fahrenheit)).not_to be_nil
+      end
+    end
+
+    context 'with wrong dimension' do
+      it 'returns correct error message' do
+        expect(subject.current_temperature(inch)).to eq error_message
+      end
+    end
   end
 
   describe '#feels_like_temperature' do
-    
+    context 'with celsium dimension' do
+      it 'returns result with a string type' do
+        expect(subject.feels_like_temperature(celsium)).to be_a String
+      end
+
+      it 'returns truthy result' do
+        expect(subject.feels_like_temperature(celsium)).not_to be_nil
+      end
+    end
+
+    context 'with kelvin dimension' do
+      it 'returns result with a string type' do
+        expect(subject.feels_like_temperature(kelvin)).to be_a String
+      end
+
+      it 'returns truthy result' do
+        expect(subject.feels_like_temperature(kelvin)).not_to be_nil
+      end
+    end
+
+    context 'with fahrenheit dimension' do
+      it 'returns result with a string type' do
+        expect(subject.feels_like_temperature(fahrenheit)).to be_a String
+      end
+
+      it 'returns truthy result' do
+        expect(subject.feels_like_temperature(fahrenheit)).not_to be_nil
+      end
+    end
+
+    context 'with wrong dimension' do
+      it 'returns correct error message' do
+        expect(subject.feels_like_temperature(inch)).to eq error_message
+      end
+    end
   end
 
   describe '#weather_description' do
-    
+    it 'returns result with a string type' do
+      expect(subject.weather_description).to be_a String
+    end
+
+    it 'returns truthy result' do
+      expect(subject.weather_description).not_to be_nil
+    end
   end
 
   describe '#humidity' do
-    
+    it 'returns result with a string type' do
+      expect(subject.humidity).to be_a String
+    end
+
+    it 'returns truthy result' do
+      expect(subject.humidity).not_to be_nil
+    end
   end
 
   describe '#pressure' do
-    
+    it 'returns result with a string type' do
+      expect(subject.pressure).to be_a String
+    end
+
+    it 'returns truthy result' do
+      expect(subject.pressure).not_to be_nil
+    end
   end
 end
