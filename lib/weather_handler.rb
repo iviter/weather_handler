cb2de17986e27a5fb7ccf9a762654b67a77f66ce
Ivan Viter <ivanviter@i.ua>
2021-05-08 19:28:03 +0300

    Refactor logic

--- lib/weather_handler.rb
+++ lib/weather_handler.rb
@@ -6,29 +6,31 @@ require 'dotenv/load'
 require 'json'
 
 module WeatherHandler
-  class Error < StandardError; end
-
   class Weather
     CONVERTATION_KELVIN_VALUE = 273.15
     CONVERTATION_FAHRENHEIT_VALUE = 459.67
     FAHRENHEIT_COEFFICIENT = 1.8
-    ERROR_MESSAGE = 'Please, try to type correct dimension'
+    ERROR_MESSAGE = 'Please, try to type correct dimension. Correct types are: Celsium, Kelvin and Fahrenheit.'
+    CELSIUM = 'celsium'
+    KELVIN = 'kelvin'
+    FAHRENHEIT = 'fahrenheit'
 
     def initialize(city)
       @city = city
     end
 
     def current_temperature(dimension)
-      case dimension.downcase
-      when 'celsium'
-        "#{degrees_c} C"
-      when 'kelvin'
-        "#{degrees_k} K"
-      when 'fahrenheit'
-        "#{degrees_f} F"
-      else
-        ERROR_MESSAGE
-      end
+      converter(dimension)
+    end
+
+    def feels_like_temperature(dimension)
+      amount = (JSON.parse(weather_data.body)['main']['feels_like']).round(1)
+
+      converter(dimension, amount)
+    end
+
+    def weather_description
+      JSON.parse(weather_data.body)['weather'].first['description'].split.map(&:capitalize).join(' ')
     end
 
     def humidity
@@ -37,14 +39,6 @@ module WeatherHandler
 
     def pressure
       "#{JSON.parse(weather_data.body)['main']['pressure']} mb"
-    end
-
-    def weather_description
-      JSON.parse(weather_data.body)['weather'].first['description'].split.map(&:capitalize).join(' ')
-    end
-
-    def approximate_weather
-      (JSON.parse(weather_data.body)['main']['feels_like']).round(1)
     end
 
     private
@@ -59,16 +53,29 @@ module WeatherHandler
       "api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV['OPEN_WEATHER_API_KEY']}"
     end
 
-    def degrees_k
+    def converter(dimension, quantity = degrees_in_k)
+      case dimension.downcase
+      when CELSIUM
+        "#{degrees_to_c(quantity)} C"
+      when KELVIN
+        "#{quantity} K"
+      when FAHRENHEIT
+        "#{degrees_to_f(quantity)} F"
+      else
+        ERROR_MESSAGE
+      end
+    end
+
+    def degrees_to_c(quantity)
+      (quantity - CONVERTATION_KELVIN_VALUE).round(1)
+    end
+
+    def degrees_in_k
       (JSON.parse(weather_data.body)['main']['temp']).round(1)
     end
 
-    def degrees_c
-      (degrees_k - CONVERTATION_VALUE).round(1)
-    end
-
-    def degrees_f
-      (degrees_k * FAHRENHEIT_COEFFICIENT - CONVERTATION_FAHRENHEIT_VALUE).round(1)
+    def degrees_to_f(quantity)
+      (quantity * FAHRENHEIT_COEFFICIENT - CONVERTATION_FAHRENHEIT_VALUE).round(1)
     end
   end
 end
